0e2d9a51ef5d1efbf4b384c33d98e5fe4761cec9
Frieder Bluemle <frieder.bluemle@gmail.com>
2017-05-10 19:18:02 +0800

    Add ANDROID_HOME support for macOS

--- source/50-android-sdk.sh
+++ source/50-android-sdk.sh
@@ -2,4 +2,10 @@ ANDROID_HOME=/opt/android-sdk-linux
 if [[ -d $ANDROID_HOME ]]; then
   export ANDROID_HOME
   export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH
+else
+  ANDROID_HOME=/Users/$USER/Library/Android/sdk
+  if [[ -d $ANDROID_HOME ]]; then
+    export ANDROID_HOME
+    export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH
+  fi
 fi
