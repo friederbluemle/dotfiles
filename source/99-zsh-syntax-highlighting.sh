80c903800480135c724019cf6dcd3b7730420216
Frieder Bluemle <frieder.bluemle@gmail.com>
2018-02-24 11:20:48 +0800

    Do not load zsh-syntax-highlighting for non zsh

--- source/99-zsh-syntax-highlighting.sh
+++ source/99-zsh-syntax-highlighting.sh
@@ -1,7 +1,9 @@
-if [[ -f "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
-  source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-else
-  if [[ -f "/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]] then
-    source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [ -n "$ZSH_VERSION" ]; then
+  if [[ -f "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
+    source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+  else
+    if [[ -f "/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
+      source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    fi
   fi
 fi
