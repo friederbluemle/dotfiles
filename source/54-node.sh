c219fb54da90fa15bf37d1953fa3105d78c1ad89
Frieder Bluemle <frieder.bluemle@gmail.com>
2018-11-18 17:01:09 +0800

    Update node/nvm script

--- source/54-node.sh
+++ source/54-node.sh
@@ -1,15 +1,12 @@
-NVM_DIR=$HOME/.nvm
-if [[ -d $NVM_DIR ]]; then
-  export NVM_DIR
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-else
-  NVM_DIR=/usr/share/nvm
-  if [[ -d $NVM_DIR ]]; then
-    source /usr/share/nvm/init-nvm.sh
-  fi
-fi
 NODE_MODULES_DIR=$HOME/.node_modules
 if [[ -d $NODE_MODULES_DIR ]]; then
-  export npm_config_prefix=$NODE_MODULES_DIR
-  PATH="$NODE_MODULES_DIR/bin:$PATH"
+  if [[ -z "$npm_config_prefix" ]]; then
+    export npm_config_prefix=$NODE_MODULES_DIR
+    PATH="$NODE_MODULES_DIR/bin:$PATH"
+  fi
+else
+  NVM_SOURCE=/usr/share/nvm
+  if [[ -d $NVM_SOURCE ]]; then
+    [ -s "$NVM_SOURCE/init-nvm.sh" ] && . "$NVM_SOURCE/init-nvm.sh"
+  fi
 fi
