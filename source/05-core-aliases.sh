c94c5255447ff8e0a75a2335047a41bb0790efcd
Frieder Bluemle <frieder.bluemle@gmail.com>
2018-11-23 17:58:57 +0800

    Change git pager to diff-so-fancy

--- source/05-core-aliases.sh
+++ source/05-core-aliases.sh
@@ -1,1 +1,2 @@
 alias initvimplug='curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+alias initgitdiff='curl -fLo ~/bin/diff-so-fancy --create-dirs https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/diff-so-fancy'
