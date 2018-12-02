c8d08252ec1b5255b94c0d7ae429dfb719f27a36
Frieder Bluemle <frieder.bluemle@gmail.com>
2018-12-02 19:22:31 +0800

    Set ANDROID_HOME only once

--- source/50-android-sdk.sh
+++ source/50-android-sdk.sh
@@ -1,14 +1,16 @@
-PATHS=(
-  "/opt/android-sdk"
-  "/opt/android-sdk-linux"
-  "/Users/$USER/Library/Android/sdk"
-)
+if [[ -z $ANDROID_HOME ]]; then
+  PATHS=(
+    "/opt/android-sdk"
+    "/opt/android-sdk-linux"
+    "/Users/$USER/Library/Android/sdk"
+  )
 
-for p in "${PATHS[@]}"
-do
-  if [[ -d $p ]]; then
-    export ANDROID_HOME=$p
-    PATH=$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH
-    break
-  fi
-done
+  for p in "${PATHS[@]}"
+  do
+    if [[ -d $p ]]; then
+      export ANDROID_HOME=$p
+      PATH=$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH
+      break
+    fi
+  done
+fi
