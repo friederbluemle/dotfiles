812af3131dc561baa908c3215dc645480c931e54
Frieder Bluemle <frieder.bluemle@gmail.com>
2021-05-03 12:46:51 -0600

    Update wezterm config

--- manual/config/wezterm/wezterm.lua
+++ manual/config/wezterm/wezterm.lua
@@ -1,2 +1,11 @@
 return {
+  enable_scroll_bar = true,
+  font_size = 13.7,
+  harfbuzz_features = {"calt=0", "clig=0", "liga=0"},
+  initial_cols = 100,
+  initial_rows = 32,
+  line_height = 0.85,
+  scrollback_lines = 6000,
+  selection_word_boundary = " \t\n{}[]()\"'`.,;:=",
+  window_decorations = "RESIZE",
 }
