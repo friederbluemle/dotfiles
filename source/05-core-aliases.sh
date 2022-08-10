21f72282cc157be237991ea9895d523a6df25f40
Frieder Bluemle <frieder.bluemle@gmail.com>
2022-08-10 01:30:32 -0700

    Update 05-core-aliases.sh

--- source/05-core-aliases.sh
+++ source/05-core-aliases.sh
@@ -1,7 +1,8 @@
-alias inithomedirs='mkdir -p $HOME/{bin,github,tmp}'
-alias initvimplug='mkdir -p $HOME/.vim/autoload && git clone git://github.com/junegunn/vim-plug.git $HOME/github/junegunn/vim-plug && ln -s $HOME/github/junegunn/vim-plug/plug.vim $HOME/.vim/autoload/plug.vim'
-alias initgitdiff='git clone git://github.com/so-fancy/diff-so-fancy.git $HOME/github/so-fancy/diff-so-fancy && ln -s $HOME/github/so-fancy/diff-so-fancy/diff-so-fancy $HOME/bin/diff-so-fancy'
-alias initdotfiles='git clone git://github.com/friederbluemle/dotfiles.git $HOME/github/friederbluemle/dotfiles && ln -s $HOME/github/friederbluemle/dotfiles $HOME/.dotfiles'
-alias initomz='git clone git://github.com/ohmyzsh/ohmyzsh.git $HOME/github/ohmyzsh/ohmyzsh && ln -s $HOME/github/ohmyzsh/ohmyzsh $HOME/.oh-my-zsh'
-alias initomzfork='pushd $HOME/github/ohmyzsh/ohmyzsh && git remote add fb https://github.com/friederbluemle/ohmyzsh.git && git fetch fb && git checkout fb-custom && popd'
-alias initzshrc='ln -s $HOME/github/ohmyzsh/ohmyzsh/templates/zshrc.zsh-template $HOME/.zshrc'
+src=$HOME/src
+alias inithomedirs="mkdir -p $HOME/{bin,src,tmp,wsrc}"
+alias initvimplug="mkdir -p $HOME/.vim/autoload && git clone git://github.com/junegunn/vim-plug.git $src/junegunn/vim-plug && ln -s $src/junegunn/vim-plug/plug.vim $HOME/.vim/autoload/plug.vim"
+alias initgitdiff="git clone git://github.com/so-fancy/diff-so-fancy.git $src/so-fancy/diff-so-fancy && ln -s $src/so-fancy/diff-so-fancy/diff-so-fancy $HOME/bin/diff-so-fancy"
+alias initdotfiles="git clone git://github.com/friederbluemle/dotfiles.git $src/friederbluemle/dotfiles && ln -s $src/friederbluemle/dotfiles $HOME/.dotfiles"
+alias initomz="git clone git://github.com/ohmyzsh/ohmyzsh.git $src/ohmyzsh/ohmyzsh && ln -s $src/ohmyzsh/ohmyzsh $HOME/.oh-my-zsh"
+alias initomzfork="pushd $src/ohmyzsh/ohmyzsh && git remote add fb https://github.com/friederbluemle/ohmyzsh.git && git fetch fb && git checkout fb-custom && popd"
+alias initzshrc="ln -s $src/ohmyzsh/ohmyzsh/templates/zshrc.zsh-template $HOME/.zshrc"
