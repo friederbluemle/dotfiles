daf9df686926d01619107a041700df29c5b27641
Frieder Bluemle <frieder.bluemle@gmail.com>
2024-05-16 22:20:11 -0700

    Add new homebrew location zsh-syntax-highlighting

--- source/95-zsh-plugins.sh
+++ source/95-zsh-plugins.sh
@@ -5,12 +5,18 @@ if [ -n "$ZSH_VERSION" ]; then
     if [[ -f "/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
       source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
     fi
+    if [[ -f "/opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
+      source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    fi
   fi
   if [[ -f "/usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh" ]]; then
     source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
   else
     if [[ -f "/usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh" ]]; then
       source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+    fi
+    if [[ -f "/opt/homebrew/share/zsh-history-substring-search/zsh-history-substring-search.zsh" ]]; then
+      source /opt/homebrew/share/zsh-history-substring-search/zsh-history-substring-search.zsh
     fi
   fi
 fi
