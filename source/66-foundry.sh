4061c010df7495d6ac966e9f7d2c2c65fd99100d
Frieder Bluemle <frieder.bluemle@gmail.com>
2025-10-06 23:38:28 -0700

    Add Foundry and JetBrains paths

---
+++ source/66-foundry.sh
@@ -0,0 +1,4 @@
+PREFIX="$HOME/.foundry"
+if [ -d "$PREFIX/bin" ]; then
+  PATH="$PATH:$PREFIX/bin"
+fi
---
+++ source/67-jetbrains.sh
@@ -0,0 +1,4 @@
+PREFIX="$HOME/Library/Application Support/JetBrains/Toolbox"
+if [ -d "$PREFIX/scripts" ]; then
+  PATH="$PATH:$PREFIX/scripts"
+fi
