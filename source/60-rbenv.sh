9255c8d282a2fe5fe277d3c8b67ae9ec3d87e443
Frieder Bluemle <frieder.bluemle@gmail.com>
2026-07-12 14:55:10 -0700

    Lazy-load rbenv on first use

--- source/60-rbenv.sh
+++ source/60-rbenv.sh
@@ -1,3 +1,10 @@
+# Shims alone make ruby/gem/bundle resolve; defer the rbenv init fork
+# until the rbenv command itself is first used.
 if command -v rbenv >/dev/null; then
-  eval "$(rbenv init - --no-rehash zsh)"
+  export PATH="${RBENV_ROOT:-$HOME/.rbenv}/shims:$PATH"
+  rbenv() {
+    unset -f rbenv
+    eval "$(command rbenv init - --no-rehash zsh)"
+    rbenv "$@"
+  }
 fi
