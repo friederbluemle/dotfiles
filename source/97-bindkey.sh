9ebdaa78b18a03518851d53df0dca920cd5e73e6
Frieder Bluemle <frieder.bluemle@gmail.com>
2026-07-15 02:09:47 -0700

    Add menuselect keybindings

--- source/97-bindkey.sh
+++ source/97-bindkey.sh
@@ -1,2 +1,9 @@
 bindkey -M emacs '^P' history-substring-search-up
 bindkey -M emacs '^N' history-substring-search-down
+
+# Completion menu selection: Enter executes the command line right away,
+# Ctrl-J/Ctrl-K move the selection (fzf-style). Typing any ordinary
+# character (e.g. Space) accepts the highlight and continues editing.
+bindkey -M menuselect '^M' .accept-line
+bindkey -M menuselect '^J' down-line-or-history
+bindkey -M menuselect '^K' up-line-or-history
